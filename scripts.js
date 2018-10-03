8b151326fdf53d54c22390ce7e04967942cc6f4a
Camila Santos <acamilass@gmail.com>
2018-10-03 20:25:31 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+              
+    $("p").on("click", function() {
+        alert("Action is working");
+    });
+
+    $("#btn1").on("click", function(){
+        alert("First button is working");
+        $(this).css("color", "red");                    
+    });
+
+    $("#btn2").on("click", function(){
+        alert("Second button is working");
+        $(this).css("background-color", "blue");
+    });
+
+    $("#btn3").on("click", function(){
+        alert("Third button is working");
+        $(this).css("text-transform", "uppercase");
+    })
+});
